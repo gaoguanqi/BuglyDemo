9d7309c99c39f014954a6a76352a695bfd7b382f
高冠祺 <gaoguanqi@xiaoxinyong.com>
2017-12-06 16:47:32 +0800

    测试支持新增非export的Activity

--- app/src/main/java/com/maple/smaple/buglydemo/MainActivity.java
+++ app/src/main/java/com/maple/smaple/buglydemo/MainActivity.java
@@ -1,12 +1,16 @@
 package com.maple.smaple.buglydemo;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.maple.smaple.buglydemo.home.HomeActivity;
 import com.yanzhenjie.permission.AndPermission;
 import com.yanzhenjie.permission.Permission;
 import com.yanzhenjie.permission.PermissionNo;
@@ -29,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView mTextView;
     private Context ctx;
     private static final int REQUEST_CODE_PERMISSION_MULTI = 101;
+
+    private Button mButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,8 +58,17 @@ public class MainActivity extends AppCompatActivity {
                 })
                 .start();
 
-        mTextView = findViewById(R.id.textView);
+        mTextView = (TextView)findViewById(R.id.textView);
         mTextView.setText("修复成功啦！");
+
+
+        mButton = (Button)findViewById(R.id.button);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ctx,HomeActivity.class));
+            }
+        });
     }
 
 
